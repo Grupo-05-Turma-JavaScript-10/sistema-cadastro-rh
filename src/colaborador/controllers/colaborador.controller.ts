94b5b4d03113bb82553adaed5d69633e9bcdde66
Eduardo <eduardoribeirodefraga05@gmail.com>
2025-12-18 12:01:49 -0300

    feat: adiciona controller de colaborador

---
+++ src/colaborador/controllers/colaborador.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, HttpCode, HttpStatus, Get } from "@nestjs/common";
+import { ColaboradorService } from "../services/colaborador.service";
+import { Colaborador } from "../entities/colaborador.entity";
+
+@Controller("/colaboradores")
+export class ColaboradorController {
+    constructor(private readonly colaboradorService: ColaboradorService) {}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Colaborador[]> {
+        return this.colaboradorService.findAll();
+    }
+}
