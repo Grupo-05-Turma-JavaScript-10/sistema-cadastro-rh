23f19434aeb838e82c41d213d1351f17cc817903
Eduardo <eduardoribeirodefraga05@gmail.com>
2025-12-18 12:03:40 -0300

    feat: registra módulo de colaborador

--- src/colaborador/colaborador.module.ts
+++ src/colaborador/colaborador.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Colaborador } from "./entities/colaborador.entity";
+import { ColaboradorService } from "./services/colaborador.service";
+import { ColaboradorController } from "./controllers/colaborador.controller";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Colaborador])],
-    providers: [],
-    controllers: [],
+    providers: [ColaboradorService],
+    controllers: [ColaboradorController],
     exports: [],
 })
 
